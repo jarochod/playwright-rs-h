8885afd74bc93920198bb9f5be34f7ad6f500856
Jaroslaw Chodubski <jaro.chod@gmail.com.com>
2024-06-21 17:10:40 +0200

    modify 1 file | 9. Assert text changes

--- tests/product_page_add_item.spec.js
+++ tests/product_page_add_item.spec.js
@@ -1,9 +1,16 @@
-import { test } from "@playwright/test"
+import { test, expect } from "@playwright/test"
 
 test("Product Page Add to Basket", async ({ page }) => {
     await page.goto("localhost:2221");
 
-    const addToBasketButton = page.getByRole('button', { name: 'Add!!! to Basket' }).first()
+    const addToBasketButton = page.locator('[data-qa="product-button"]').first()
+    const basketCounter = page.locator('[data-qa="header-basket-count"]');
+
     await addToBasketButton.waitFor()
+    await expect(addToBasketButton).toHaveText("Add to Basket");
+    await expect(basketCounter).toHaveText("0");
     await addToBasketButton.click()
+    await expect(addToBasketButton).toHaveText("Remove from Basket");
+    await expect(basketCounter).toHaveText("1");
+
 })
