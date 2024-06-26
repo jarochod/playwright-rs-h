dae00010bd7dab14943a63ef66845edb884314cb
Jaroslaw Chodubski <jaro.chod@gmail.com.com>
2024-06-26 14:39:12 +0200

    modify 2 files | Ch.13

--- page-objects/ProoductsPage.js
+++ page-objects/ProoductsPage.js
@@ -1,9 +1,16 @@
 export class ProductsPage {
     constructor(page) {
         this.page = page
+
+        this.addButtons = page.locator('[data-qa="product-button"]')
     }
 
     visit = async () => {
         await this.page.goto("/")
+    }
+
+    addProductToBasket = async (index) => {
+        await this.addButtons.nth(index).waitFor()
+        await this.addButtons.nth(index).click()
     }
 }
