3fb57a3db21c865c6fc54f37e24a4ab220f7b055
Jaroslaw Chodubski <jaro.chod@gmail.com.com>
2024-06-27 02:28:47 +0200

    added 1 file, modify 2 files | Ch.16

---
+++ page-objects/NavigationPage.js
@@ -0,0 +1,13 @@
+export class NavigationPage {
+    constructor(page) {
+        this.page = page
+
+        this.basketCounter = page.locator('[data-qa="header-basket-count"]')
+    }
+
+    getBasketCount = async () => {
+        await this.basketCounter.waitFor()
+        const text = await this.basketCounter.innerText()
+        return parseInt(text, 10)
+    }
+}
--- page-objects/ProoductsPage.js
+++ page-objects/ProoductsPage.js
@@ -1,31 +1,26 @@
 import { expect } from "@playwright/test"
+import { NavigationPage } from "./NavigationPage.js"
 
 export class ProductsPage {
     constructor(page) {
         this.page = page
 
         this.addButtons = page.locator('[data-qa="product-button"]')
-        this.basketCounter = page.locator('[data-qa="header-basket-count"]')
     }
 
     visit = async () => {
         await this.page.goto("/")
     }
 
-    getBasketCount = async () => {
-        await this.basketCounter.waitFor()
-        const text = await this.basketCounter.innerText()
-        return parseInt(text, 10)
-    }
-
     addProductToBasket = async (index) => {
         const specificAddButon = this.addButtons.nth(index)
         await specificAddButon.waitFor()
         await expect(specificAddButon).toHaveText("Add to Basket")
-        const basketCounterBeforeAdding = await this.getBasketCount()
+        const navigationPage = new NavigationPage(this.page)
+        const basketCounterBeforeAdding = await navigationPage.getBasketCount()
         await specificAddButon.click()
         await expect(specificAddButon).toHaveText("Remove from Basket")
-        const basketCounterAfterAdding = await this.getBasketCount()
+        const basketCounterAfterAdding = await navigationPage.getBasketCount()
         expect(basketCounterAfterAdding).toBeGreaterThan(basketCounterBeforeAdding)
     }
 }
