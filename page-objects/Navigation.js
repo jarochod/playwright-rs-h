b726063d2cfc26dbe6e31f071f24233dc64da4de
Jaroslaw Chodubski <jaro.chod@gmail.com.com>
2024-06-29 00:53:14 +0200

    modify 2 files | Ch.17

--- page-objects/Navigation.js
+++ page-objects/Navigation.js
@@ -3,11 +3,18 @@ export class Navigation {
         this.page = page
 
         this.basketCounter = page.locator('[data-qa="header-basket-count"]')
+        this.checkoutLink = page.getByRole('link', { name: 'Checkout' })
     }
 
     getBasketCount = async () => {
         await this.basketCounter.waitFor()
         const text = await this.basketCounter.innerText()
         return parseInt(text, 10)
+    }
+
+    goToCheckout = async () => {
+        await this.checkoutLink.waitFor()
+        await this.checkoutLink.click()
+        await this.page.waitForURL("/basket")
     }
 }
--- tests/new_user_full_journey.spec.js
+++ tests/new_user_full_journey.spec.js
@@ -1,5 +1,6 @@
 import { test} from "@playwright/test"
 import { ProductsPage } from "../page-objects/ProoductsPage.js"
+import { Navigation } from "../page-objects/Navigation.js"
 
 test.only("New user full end-to-end test journey", async ({ page }) => {
     // ProductsPage.visit()
@@ -8,4 +9,7 @@ test.only("New user full end-to-end test journey", async ({ page }) => {
     await productsPage.addProductToBasket(0)
     await productsPage.addProductToBasket(1)
     await productsPage.addProductToBasket(2)
+    const navigation = new Navigation(page)
+    await navigation.goToCheckout()
+    await page.pause()
 })
