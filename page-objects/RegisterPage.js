f586e93efa6865232cb89d4cf35c79fd03088c60
Jaroslaw Chodubski <jaro.chod@gmail.com.com>
2024-07-02 17:38:23 +0200

    modify 3 files | Ch.23

--- page-objects/RegisterPage.js
+++ page-objects/RegisterPage.js
@@ -1,17 +1,26 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export class RegisterPage {
     constructor(page) {
         this.page = page
+        
         this.emailInput = page.getByPlaceholder('E-Mail')
         this.passwordInput = page.getByPlaceholder('Password')
         this.registerButton = page.getByRole('button', { name: 'Register' })
     }
 
     singUpAsNewUser = async () => {
+        // type into email input
         await this.emailInput.waitFor()
-        await this.emailInput.fill("testymctesterson@testers.com")
-        await this.passwordInput.fill("supersecretpassword")
+        const emailId = uuidv4();
+        const email = emailId + "@gmail.com"
+        await this.emailInput.fill(email)
+        // type into password input
+        await this.passwordInput.waitFor()
+        const password = uuidv4();
+        await this.passwordInput.fill(password)
+        // click register button
         await this.registerButton.waitFor()
         await this.registerButton.click()
-        await this.page.pause()
     }
 }
