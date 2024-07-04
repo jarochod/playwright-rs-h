ba29f8653e15a8187d5112c9abecefe9b53c841a
Jaroslaw Chodubski <jaro.chod@gmail.com.com>
2024-07-04 21:25:04 +0200

    modify 1 file | Ch.31

--- page-objects/PaymentPage.js
+++ page-objects/PaymentPage.js
@@ -1,13 +1,19 @@
+import { expect } from "@playwright/test"
+
 export class PaymentPage {
     constructor(page) {
         this.page = page
 
         this.discountCode = page.frameLocator('[data-qa="active-discount-container"]')
                                 .locator('[data-qa="discount-code"]')
+        this.discountInput = page.getByPlaceholder('Discount code')
     }
 
     activateDiscount = async () => {
         await this.discountCode.waitFor()
-        await this.page.pause()
+        const code = await this.discountCode.innerText()
+        await this.discountInput.waitFor()
+        await this.discountInput.fill(code)
+        await expect(this.discountInput).toHaveValue(code)
     }
 }
