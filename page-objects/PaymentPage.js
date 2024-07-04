ee9d8ef3baa75cdb9c24f264d276a3300b39fe39
Jaroslaw Chodubski <jaro.chod@gmail.com.com>
2024-07-05 01:02:55 +0200

    modify 1 file | Ch.32

--- page-objects/PaymentPage.js
+++ page-objects/PaymentPage.js
@@ -13,7 +13,14 @@ export class PaymentPage {
         await this.discountCode.waitFor()
         const code = await this.discountCode.innerText()
         await this.discountInput.waitFor()
+
+        // Option 1 for laggy inputs: using .fill() with await expect()
         await this.discountInput.fill(code)
         await expect(this.discountInput).toHaveValue(code)
+
+        // Option 2 for laggy inputs: slow typing
+        // await this.discountInput.focus()
+        // await this.page.keyboard.type(code, {delay: 1000})
+        // expect(await this.discountInput.inputValue()).toBe(code)
     }
 }
