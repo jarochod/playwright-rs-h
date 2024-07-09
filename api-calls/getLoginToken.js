6b45d9796480b3c5c6f181daf164edca7f62da2c
Jaroslaw Chodubski <jaro.chod@gmail.com.com>
2024-07-09 16:12:53 +0200

    modify 1 file | Ch.44

--- api-calls/getLoginToken.js
+++ api-calls/getLoginToken.js
@@ -1,7 +1,13 @@
 import * as nodeFetch from "node-fetch"
+
 export const getLoginToken = async () => {
     const reponse = await nodeFetch("http://localhost:2221/api/login", {
-        metod: "POST",
-        body: {"username": "admin", "password": "Admin123"}
+        method: "POST",
+        body: JSON.stringify({"username":"admin","password":"Admin123"}),
     })
+    if (reponse.status != 200) {
+        throw new Error("An error occured trying to retrieve the login token.")
+    }
+    const body = await reponse.json()
+    return body.token
 }
